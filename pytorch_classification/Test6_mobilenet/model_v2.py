a060d686e88f92cb5920ceecc1caa3a32d77fc8d
infwolad <cosz@vip.qq.com>
2022-02-28 21:59:25 +0800

    2.28.22

--- pytorch_classification/Test6_mobilenet/model_v2.py
+++ pytorch_classification/Test6_mobilenet/model_v2.py
@@ -2,14 +2,14 @@ from torch import nn
 import torch
 
 
-def _make_divisible(ch, divisor=8, min_ch=None):
+def _make_divisible(ch, divisor=8, min_ch=None):  # 将输入的channel最接近8的倍数进行取整
     """
     This function is taken from the original tf repo.
     It ensures that all layers have a channel number that is divisible by 8
     It can be seen here:
     https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
     """
-    if min_ch is None:
+    if min_ch is None:  # 来源于TensorFlow
         min_ch = divisor
     new_ch = max(min_ch, int(ch + divisor / 2) // divisor * divisor)
     # Make sure that round down does not go down by more than 10%.
@@ -18,51 +18,51 @@ def _make_divisible(ch, divisor=8, min_ch=None):
     return new_ch
 
 
-class ConvBNReLU(nn.Sequential):
+class ConvBNReLU(nn.Sequential):  # 该部分为无残差结构的，sequential和module的区别见书签保存
     def __init__(self, in_channel, out_channel, kernel_size=3, stride=1, groups=1):
-        padding = (kernel_size - 1) // 2
-        super(ConvBNReLU, self).__init__(
-            nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding, groups=groups, bias=False),
+        padding = (kernel_size - 1) // 2  # 计算padding值
+        super(ConvBNReLU, self).__init__(  # sequential无需forward函数，但是init中必须按照顺序
+            nn.Conv2d(in_channel, out_channel, kernel_size, stride, padding, groups=groups, bias=False),  # bias在这里没有作用，于是关闭
             nn.BatchNorm2d(out_channel),
             nn.ReLU6(inplace=True)
         )
 
 
-class InvertedResidual(nn.Module):
+class InvertedResidual(nn.Module):  # 该部分为倒残差函数
     def __init__(self, in_channel, out_channel, stride, expand_ratio):
         super(InvertedResidual, self).__init__()
-        hidden_channel = in_channel * expand_ratio
-        self.use_shortcut = stride == 1 and in_channel == out_channel
+        hidden_channel = in_channel * expand_ratio  # expend是relu中那个放大倍数，如这里是6/1
+        self.use_shortcut = stride == 1 and in_channel == out_channel  # bowl值
 
-        layers = []
-        if expand_ratio != 1:
+        layers = []  # 预先定义一个数组
+        if expand_ratio != 1:  # 看是不是第一层，因为第一层bottleneck的t为1
             # 1x1 pointwise conv
             layers.append(ConvBNReLU(in_channel, hidden_channel, kernel_size=1))
-        layers.extend([
+        layers.extend([  # 和append效果类似，直接多层
             # 3x3 depthwise conv
             ConvBNReLU(hidden_channel, hidden_channel, stride=stride, groups=hidden_channel),
-            # 1x1 pointwise conv(linear)
+            # 1x1 pointwise conv(linear)  无需添加linear，默认即是
             nn.Conv2d(hidden_channel, out_channel, kernel_size=1, bias=False),
-            nn.BatchNorm2d(out_channel),
+            nn.BatchNorm2d(out_channel),  # BN网络
         ])
 
-        self.conv = nn.Sequential(*layers)
+        self.conv = nn.Sequential(*layers)  # 将上面压入conv
 
     def forward(self, x):
-        if self.use_shortcut:
+        if self.use_shortcut:  # 判断是否有残差结构
             return x + self.conv(x)
         else:
             return self.conv(x)
 
 
-class MobileNetV2(nn.Module):
+class MobileNetV2(nn.Module):  # 主体网络
     def __init__(self, num_classes=1000, alpha=1.0, round_nearest=8):
         super(MobileNetV2, self).__init__()
         block = InvertedResidual
         input_channel = _make_divisible(32 * alpha, round_nearest)
         last_channel = _make_divisible(1280 * alpha, round_nearest)
 
-        inverted_residual_setting = [
+        inverted_residual_setting = [  # 对应表格
             # t, c, n, s
             [1, 16, 1, 1],
             [6, 24, 2, 2],
@@ -86,7 +86,7 @@ class MobileNetV2(nn.Module):
         # building last several layers
         features.append(ConvBNReLU(input_channel, last_channel, 1))
         # combine feature layers
-        self.features = nn.Sequential(*features)
+        self.features = nn.Sequential(*features)  # 压入
 
         # building classifier
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
